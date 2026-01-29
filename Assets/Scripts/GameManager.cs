c48cc1d57c240674dbba914b232b5cfaec936da1
a19albertons <a19albertons@iessanclemente.net>
2026-01-29 13:25:04 +0100

    Comentario inicial

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance;
+
+    const int LIVES = 3;
+
+    [SerializeField]
+    TextMeshProUGUI txtScore;
+
+    [SerializeField]
+    TextMeshProUGUI txtMaxScore;
+
+    [SerializeField]
+    TextMeshProUGUI txtMessage;
+
+    //Array paara las imágenes que marcan las vidas
+    [SerializeField]
+    GameObject[] imgLives;
+
+    // Método estático para obtener la instancia del GameManager
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    // Función Awake se ejecuta cuando se instancia el objeto
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Evitar que el objeto se destruya al cambiar de escena
+        }
+        else if (instance != this)
+        {
+            // Si ya existe una instancia, destruimos el nuevo GameManager para mantener la singularidad
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    // Velocidad de los disparos
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    GameObject hit; // Efecto de impacto al colisionar
+
+    // Tiempo que duran los disparos antes de autodestruirse
+    [SerializeField]
+    float lifetime;
+
+    void Start()
+    {
+        // Destruir el disparo después de un cierto tiempo
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        // Mover el disparo hacia arriba
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    // Método para destruir el disparo cuando sale de la pantalla
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Instantiate(hit, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        // Debug.Log("Colisión con disparo");
+    }
+}
